cf676621824a0f45d8b97050837bafa573667935
aditya vinayak <adityavinayak@adityas-MacBook-Air.local>
2022-12-06 18:26:45 +0530

    added invitor name

--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,7 @@ export const inviteNewUser = async (req, res, next) => {
         },
       });
       let sId = userInfo.userId;
-      let redirectedUrl = `https://buggenix.netlify.app/team-invite?email=${to}`;
+      let redirectedUrl = `https://buggenix.netlify.app/team-invite?email=${to}&invitor=${invitor}`;
 
       const mailOptions = {
         from: from,
